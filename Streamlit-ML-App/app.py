190c77b794126504a9b366d59d207423ffadfe48
prabodhw96 <prabodhwankhede1996@gmail.com>
2020-06-04 18:16:53 +0530

    Load dataset

---
+++ Streamlit-ML-App/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve, precision_score, recall_score
+
+def main():
+	st.title("Binary Classification Web App")
+	st.sidebar.title("Binary Classification Web App")
+	st.markdown("Are your mushrooms edible or poisonous?")
+	st.sidebar.markdown("Are your mushrooms edible or poisonous?")
+
+	@st.cache(persist=True)
+	def load_data():
+		data = pd.read_csv("mushrooms.csv")
+		label = LabelEncoder()
+		for col in data.columns:
+			data[col] = label.fit_transform(data[col])
+		return data
+
+	df = load_data()
+
+	if st.sidebar.checkbox("Show raw data", False):
+		st.subheader("Mushroom Data Set (Classification)")
+		st.write(df)
+
+
+
+if __name__ == '__main__':
+	main()
