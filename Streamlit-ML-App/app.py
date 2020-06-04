05876bc84c9109835c5dfb4ad05024599f9bb13a
prabodhw96 <prabodhwankhede1996@gmail.com>
2020-06-04 19:51:05 +0530

    Add SVM

--- Streamlit-ML-App/app.py
+++ Streamlit-ML-App/app.py
@@ -22,7 +22,52 @@ def main():
 			data[col] = label.fit_transform(data[col])
 		return data
 
+	@st.cache(persist=True)
+	def split(df):
+		y = df.type
+		x = df.drop(columns=['type'])
+		x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+		return x_train, x_test, y_train, y_test
+
+	def plot_metrics(metrics_list):
+		if 'Confusion Matrix' in metrics_list:
+			st.subheader("Confusion Matrix")
+			plot_confusion_matrix(model, x_test, y_test, display_labels=class_names)
+			st.pyplot()
+
+		if 'ROC Curve' in metrics_list:
+			st.subheader("ROC Curve")
+			plot_roc_curve(model, x_test, y_test)
+			st.pyplot()
+
+		if 'Precision-Recall Curve' in metrics_list:
+			st.subheader("Precision-Recall Curve")
+			plot_confusion_matrix(model, x_test, y_test)
+			st.pyplot()
+
 	df = load_data()
+	x_train, x_test, y_train, y_test = split(df)
+	class_names = ['edible', 'poisonous']
+	st.sidebar.subheader('Choose Classifier')
+	classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest"))
+
+	if classifier == 'Support Vector Machine (SVM)':
+		st.sidebar.subheader("Model Hyperparameters")
+		C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C')
+		kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
+		gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
+
+		metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
+
+		if st.sidebar.button("Classify", key='classify'):
+			model = SVC(C=C, kernel=kernel, gamma=gamma)
+			model.fit(x_train, y_train)
+			accuracy = model.score(x_test, y_test)
+			y_pred = model.predict(x_test)
+			st.write("Accuracy: ", accuracy.round(2))
+			st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+			st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+			plot_metrics(metrics)
 
 	if st.sidebar.checkbox("Show raw data", False):
 		st.subheader("Mushroom Data Set (Classification)")
